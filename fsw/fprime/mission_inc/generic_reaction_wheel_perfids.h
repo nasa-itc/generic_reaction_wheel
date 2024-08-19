55d5847d254da26f5d24a7f60ee8a1d1b340a6a6
zlynch2 <zachary.j.lynch@nasa.gov>
2024-08-19 18:50:22 +0000

    making branch and fsw/fprime organization

---
+++ fsw/fprime/mission_inc/generic_reaction_wheel_perfids.h
@@ -0,0 +1,21 @@
+/************************************************************************
+** File: 
+**  generic_reaction_wheel_perfids.h
+**
+** Purpose: 
+**  Define Generic Reaction Wheel Performance IDs
+**
+** Notes:
+**
+*************************************************************************/
+#ifndef _generic_reaction_wheel_perfids_h_
+#define _generic_reaction_wheel_perfids_h_
+
+
+#define GENERIC_RW_APP_PERF_ID              77 
+
+#endif /* _generic_reaction_wheel_perfids_h_ */
+
+/************************/
+/*  End of File Comment */
+/************************/
